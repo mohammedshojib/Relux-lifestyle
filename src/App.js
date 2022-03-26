4b8b4f299e9cd3aefa974adb640261b0a3b871c7
mohammedshojib <asmdshojib11@gmail.com>
2022-03-26 18:53:27 +0600

    final tuch

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Header from "./components/Header/Header";
+import { random } from "./components/uti.js";
 
 function App() {
   const [mouses, setMouses] = useState([]);
   const [cart, setCart] = useState([]);
+  const [choose, setChoose] = useState([]);
 
   const handleAddToCart = (mouse) => {
     const newCart = [...cart, mouse];
     setCart(newCart);
+  };
+
+  const chooseOne = (cart) => {
+    let newArray = [];
+    let num = random(cart.length);
+    let selected = cart.find((mouse) => mouse.id === num);
+
+    if (selected) {
+      newArray = [selected];
+      setChoose(newArray);
+    }
   };
 
   useEffect(() => {
@@ -34,6 +47,23 @@ function App() {
           <h3>Cart</h3>
           {cart.map((item) => (
             <h4 key={item.id}>Name: {item.name}</h4>
+          ))}
+          <button className="btn btn-primary" onClick={() => chooseOne(cart)}>
+            Choose For me
+          </button>
+
+          <button
+            className="btn btn-danger mt-1"
+            onClick={() => {
+              setCart([]);
+            }}
+          >
+            Remove all
+          </button>
+          {choose.map((singleChoose) => (
+            <div key={singleChoose.id}>
+              <h2>{singleChoose.name}</h2>
+            </div>
           ))}
         </div>
       </div>
